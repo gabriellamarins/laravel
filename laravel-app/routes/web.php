382fb0e88d8bafe5e30aa3fc195135ca301a7213
Your Name <gabriella.marinsesilva@le-campus-numerique.fr>
2021-06-23 11:29:52 +0200

    Controllers created

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view ('Home page');
+    return view ('HelloWorld');
 });
 
 Route::get('/product', function () {
