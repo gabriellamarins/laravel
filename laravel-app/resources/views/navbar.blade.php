319d6bb6ca495c359b12284c2f1a970a38629c6f
gabriellamarins <81614041+gabriellamarins@users.noreply.github.com>
2021-06-24 11:43:46 +0200

    the layout blade is working it hosts the reused parts (for now navbar and footer)
    - til here just for the home

---
+++ laravel-app/resources/views/navbar.blade.php
@@ -0,0 +1,93 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a
+            class="navbar-brand"
+            href="https://www.youtube.com/watch?v=ZyhrYis509A"
+        >
+            <img src="{{asset('photo/logo_silent.jpg')}}" alt="Logo silent"
+            /></a>
+
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.html"
+                    >Home</a
+                    >
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://discord.gg/DK4GGbfV"
+                    >Dashboard</a
+                    >
+                </li>
+                <li class="nav-item dropdown">
+                    <a
+                        class="nav-link dropdown-toggle"
+                        href="#"
+                        id="navbarDropdown"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                    >
+                        Menu
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li>
+                            <a class="dropdown-item" href="index.html#features"
+                            >FEATURES</a
+                            >
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="index.html#partenaires"
+                            >PARTENAIRES</a
+                            >
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="index.html#succes">SUCCES</a>
+                        </li>
+                        <li><a class="dropdown-item" href="Team-BS.html">TEAM</a></li>
+                        <li>
+                            <a class="dropdown-item" href="catalogue.html">CATALOGUE</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="Formulaire_2.html">NOUS CONTACTER</a>
+                        </li>
+                        <li><hr class="dropdown-divider" /></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item">
+                    <a
+                        class="nav-link disabled"
+                        href="#"
+                        tabindex="-1"
+                        aria-disabled="true"
+                    >Disabled</a
+                    >
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input
+                    class="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                />
+                <button class="btn btn-outline-success" type="submit">
+                    Search
+                </button>
+            </form>
+        </div>
+    </div>
+</nav>
