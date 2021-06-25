b23bcf00fe43e80e657d49a1a36dd1b5e7141d50
gabriellamarins <81614041+gabriellamarins@users.noreply.github.com>
2021-06-25 11:38:13 +0200

    Model Product works (showing only everything and just de names)

--- laravel-app/app/Http/Controllers/ProductController.php
+++ laravel-app/app/Http/Controllers/ProductController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Product;
+
+
 
 class ProductController extends Controller
 {
@@ -11,20 +14,16 @@ class ProductController extends Controller
     public function index(){
 //        echo "Liste des Produits";
 
+
+        $products = Product::all('name');
+
+
         $listeproduits = "Liste des Produits";
-        $products = DB::select('select * from products');
-//dd($products); le var dump de laravel
-//
-//        $arraylisteproduits = [
-//            'listeproduits' => $listeproduits,
-//
-//
-//
-//        ];
+//        $products = DB::select('select * from products');
 
 
- //      return view('product.index', ['products' => $products]);
         return view('product-lis', ['products' => $products]);
+
     }
 
     public function show($id){
